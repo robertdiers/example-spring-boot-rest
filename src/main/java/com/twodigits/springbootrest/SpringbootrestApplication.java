b4dde614a4e2d3679e662ea2ac38260b38b12205
Robert Diers <robert.diers@accenture.com>
2021-09-20 18:06:18 +0000

    update

--- src/main/java/com/twodigits/springbootrest/SpringbootrestApplication.java
+++ src/main/java/com/twodigits/springbootrest/SpringbootrestApplication.java
@@ -3,11 +3,22 @@ package com.twodigits.springbootrest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+
 @SpringBootApplication
 public class SpringbootrestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootrestApplication.class, args);
+
+		int mb = 1024 * 1024;
+		MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
+		long xmx = memoryBean.getHeapMemoryUsage().getMax() / mb;
+		long xms = memoryBean.getHeapMemoryUsage().getInit() / mb;
+		System.out.println("Initial Memory (xms) : "+xms+"mb");
+		System.out.println("Max Memory (xmx) : "+xmx+"mb");
+
 	}
 
 }
